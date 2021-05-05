daa477b2c8ab7bcafcdb95945773c336da7e2992
Irina Adamuk <grinirina032@gmail.com>
2021-05-06 00:38:18 +0300

    Communicating events add and to remove from cart

--- Lesson5/test/script.js
+++ Lesson5/test/script.js
@@ -62,9 +62,6 @@ Vue.component('product', {
                >Remove from cart
             </button>
 
-            <div class="cart">
-               <p>Cart({{ cart }})</p>
-            </div>
          </div><!-- /.product-info -->
       </div>
    `,
@@ -92,16 +89,15 @@ Vue.component('product', {
             }
          ],
          sizes: ['S', 'M', 'L', 'XL', 'XXL'],
-         cart: 0,
          onSale: true,
       }
    },
    methods: {
       addToCart() {
-         this.cart += 1
+         this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
       },
       removeFromCart() {
-         this.cart -= 1
+         this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
       },
       updateProduct(index) {
          this.selectedVariant = index
@@ -136,6 +132,19 @@ Vue.component('product', {
 let app = new Vue ({
    el: '#app',
    data: {
-      premium: true
+      premium: true,
+      cart: []
+   },
+   methods: {
+      updateCart (id) {
+         this.cart.push(id)
+      },
+      removeItem (id) {
+         for(let i = this.cart.length - 1; i>= 0; i--) {
+            if (this.cart[i] === id) {
+               this.cart.splice(i, 1);
+            }
+         }
+      }
    }
 })
