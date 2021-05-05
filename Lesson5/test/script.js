41a31e56b1cf94902f96e8a8b3f55666a4242c11
Irina Adamuk <grinirina032@gmail.com>
2021-05-05 23:29:39 +0300

    Computed Properties were added

--- Lesson5/test/script.js
+++ Lesson5/test/script.js
@@ -2,26 +2,29 @@ let app = new Vue ({
    el: '#app',
    data: {
       product: 'Socks',
+      brand: 'Veu Mastery',
       description: 'A pair of warm fuzzy socks',
-      image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+      selectedVariant: 0,
       link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-      inStock: false,
       onSale: true,
       details: ["80% cotton", "20% polyester", "Gender-neutral"],
       variants: [
          {
             variantId: 2234,
             variantColor: "green",
-            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+            variantQuantity: 10
          },
          {
             variantId: 2235,
             variantColor: "blue",
-            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'   
+            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+            variantQuantity: 0   
          }
       ],
       sizes: ['S', 'M', 'L', 'XL', 'XXL'],
       cart: 0,
+      onSale: true,
    },
    methods: {
       addToCart() {
@@ -30,8 +33,26 @@ let app = new Vue ({
       removeFromCart() {
          this.cart -= 1
       },
-      updateProduct(variantImage) {
-         this.image = variantImage
+      updateProduct(index) {
+         this.selectedVariant = index
+         console.log(index)
+      }
+   },
+   computed: {
+      title() {
+         return this.brand + ' ' + this.product
+      },
+      image() {
+         return this.variants[this.selectedVariant].variantImage
+      },
+      inStock() {
+         return this.variants[this.selectedVariant].variantQuantity
+      },
+      sale() {
+         if (this.onSale) {
+            return this.brand + ' ' + this.product + ' are on sale'
+         }
+            return this.brand + ' ' + this.product + ' are not on sale'
       }
    }
 })
