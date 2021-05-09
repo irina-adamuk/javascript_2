6aa49995f56a9d98a7d27916c4d57e80a8f714d1
Irina Adamuk <grinirina032@gmail.com>
2021-05-09 16:28:28 +0300

    All Materials of home task were changed

---
+++ Lesson5/Home_task/project/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+   el: '#app',
+   data: {
+      catalogUrl: '/catalogData.json',
+      products: [],
+      imgCatalog: 'https://place-hold.it/200x150'
+   },
+   methods: {
+      getJson(url){
+         return fetch(url)
+         .then(result => result.json())
+         .catch(error => {
+            console.log(error);
+         })
+      },
+      addProduct(product){
+         console.log(product.id_product);
+         this.getJson(`${API}/addToBasket.json`).then(data)
+      }
+   },
+
+
+});
+
